760b8da4af82182e7b9757ee2cad7e52d734219d
Neal <parrnim@gmail.com>
2026-03-26 11:04:55 -0500

    library dialogues

--- Globals/currency_manager.gd
+++ Globals/currency_manager.gd
@@ -6,26 +6,27 @@ extends Node
 #@onready var floorspace: Currency = load("res://Currencies/Houses/Floor_Space.tres")
 
 @onready var all_currencies: Array[Currency] = [
+	load("res://Currencies/Books/Books.tres"),
 	load("res://Currencies/Books/Upgrades/Bookshelf.tres"),
 	load("res://Currencies/Coins/Coins.tres"),
-	load("res://Currencies/Clockwork/Clockwork.tres"),
-	load("res://Currencies/Crystal/Crystal.tres"),
-	load("res://Currencies/Herbs/Herbs.tres"),
-	load("res://Currencies/Scrolls/Scrolls.tres"),
-	load("res://Currencies/Scrolls/Ticker/Auto_quill.tres"),
-	load("res://Currencies/Scrolls/Upgrades/Scroll_Case.tres"),
-	load("res://Currencies/Clockwork/Upgrades/Clock.tres"),
-	load("res://Currencies/Coins/Upgrades/Purse.tres"),
-	load("res://Currencies/Crystal/Upgrades/velvet_box.tres"),
 	load("res://Currencies/Coins/Upgrades/Lockbox.tres"),
+	load("res://Currencies/Coins/Upgrades/Purse.tres"),
+	load("res://Currencies/Clockwork/Clockwork.tres"),
 	load("res://Currencies/Clockwork/Tickers/Clockwork_Arm.tres"),
 	load("res://Currencies/Clockwork/Upgrades/Typewriter.tres"),
+	load("res://Currencies/Clockwork/Upgrades/Clock.tres"),
+	load("res://Currencies/Crystal/Crystal.tres"),
+	load("res://Currencies/Crystal/Upgrades/velvet_box.tres"),
 	load("res://Currencies/Herbs/Tickers/PlanterBox.tres"),
 	load("res://Currencies/Herbs/Upgrades/Herb_Cupboard.tres"),
+	load("res://Currencies/Herbs/Herbs.tres"),
 	load("res://Currencies/Houses/hovel.tres"),
 	load("res://Currencies/Houses/Big_house.tres"),
 	load("res://Currencies/Houses/Floor_Space.tres"),
 	load("res://Currencies/Houses/Dimensional_Rift.tres"),
+	load("res://Currencies/Scrolls/Scrolls.tres"),
+	load("res://Currencies/Scrolls/Ticker/Auto_quill.tres"),
+	load("res://Currencies/Scrolls/Upgrades/Scroll_Case.tres"),
 	
 	load("res://Currencies/Library/Library_Card.tres"),
 	load("res://Currencies/Library/LibraryLocation.tres"),
@@ -34,18 +35,23 @@ extends Node
 	load("res://Currencies/Library/Knowledge_Lapidamancy.tres"),
 	load("res://Currencies/Library/Knowledge_Octomancy.tres"),
 	load("res://Currencies/Library/Knowledge_Oneiromancy.tres"),
+	
 	load("res://Currencies/Library/Knowledge_Curator.tres"),
+	load("res://Currencies/Library/Curator/Currencies/Curator_Octomancy.tres"),
+	
 	load("res://Currencies/Library/Knowledge_Library_1.tres"),
 	load("res://Currencies/Library/Knowledge_poppy_1.tres"),
 	load("res://Currencies/Library/Knowledge_poppy_2.tres"),
+	load("res://Currencies/Library/Librarian/Currencies/Librarian_cyanomancy.tres"),
+	
+	load("res://Currencies/Tower/Knowledge_of_tower.tres"),
+	load("res://Currencies/Tower/Knowledge_of_tower2.tres"),
 	
 	load("res://Currencies/Research/Research.tres"),
 	load("res://Currencies/Research/Ticker/Clockwork_Dreamcatcher.tres"),
-	load("res://Currencies/Books/Books.tres"),
 	load("res://Currencies/Research/Star_Chart.tres"),
 	load("res://Currencies/Magic/Magic_Crystal.tres"),
 	load("res://Currencies/Magic/Gem_Seed.tres"),
-	load("res://Currencies/Telescope/Telescope.tres"),
 	load("res://Currencies/Magic/Magic.tres"),
 	load("res://Currencies/Magic/Rules_of_Lapidamancy.tres"),
 	load("res://Currencies/Magic/Rules_Of_Octomancy.tres"),
@@ -53,7 +59,8 @@ extends Node
 	load("res://Currencies/Magic/Rules_of_Oneiromancy.tres"),
 	load("res://Currencies/Magic/Kingly_Blue.tres"),
 	load("res://Currencies/Magic/Octagon.tres"),
-	load("res://Currencies/Magic/Mage_Locus.tres")
+	load("res://Currencies/Magic/Mage_Locus.tres"),
+	load("res://Currencies/Telescope/Telescope.tres"),
 ]
 
 func eoc_check() -> bool:
--- Globals/job_manager.gd
+++ Globals/job_manager.gd
@@ -10,6 +10,9 @@ var simple_jobs: Array[Job] = [
 	load("res://Currencies/Magic/Practice_Octomancy.tres"),
 	load("res://Currencies/Magic/Practice_Lapidomancy.tres"),
 	load("res://Currencies/Magic/Practice_Cyanomancy.tres"),
+
+
+
 	load("res://Currencies/Telescope/Chart_the_stars.tres"),
 ]
 
@@ -26,22 +29,24 @@ var trades: Array[Job] = [
 
 
 var upgrades: Array[Job] = [
+	load("res://Currencies/Books/Upgrades/Buy_Bookshelf.tres"),
+	load("res://Currencies/Coins/Upgrades/Buy_lockbox.tres"),
+	load("res://Currencies/Coins/Upgrades/buy_purse.tres"),
 	load("res://Currencies/Clockwork/Upgrades/build_clock.tres"),
 	load("res://Currencies/Clockwork/Upgrades/Build_typewriter.tres"),
-	load("res://Currencies/Coins/Upgrades/buy_purse.tres"),
 	load("res://Currencies/Crystal/Upgrades/buy_velvet_box.tres"),
-	load("res://Currencies/Coins/Upgrades/Buy_lockbox.tres"),
-	load("res://Currencies/Books/Upgrades/Buy_Bookshelf.tres"),
-	load("res://Currencies/Telescope/Construct_telescope.tres"),
-	load("res://Currencies/Magic/Research_Octomancy.tres"),
+	load("res://Currencies/Herbs/Upgrades/Buy_Herb_Cupboard.tres"),
 	load("res://Currencies/Magic/Create_Octagon.tres"),
 	load("res://Currencies/Magic/Create_Gem_Seed.tres"),
+	load("res://Currencies/Magic/Craft_Mage_Locus.tres"),
+	
+	load("res://Currencies/Magic/Research_Octomancy.tres"),
 	load("res://Currencies/Magic/Research_Lapidomancy.tres"),
-	load("res://Currencies/Herbs/Upgrades/Buy_Herb_Cupboard.tres"),
-	load("res://Currencies/Scrolls/Upgrades/Buy_Scroll_Case.tres"),
 	load("res://Currencies/Magic/Research_Cyanomancy.tres"),
 	load("res://Currencies/Magic/Research_Oneiromancy.tres"),
-	load("res://Currencies/Magic/Craft_Mage_Locus.tres")
+	
+	load("res://Currencies/Scrolls/Upgrades/Buy_Scroll_Case.tres"),
+	load("res://Currencies/Telescope/Construct_telescope.tres"),
 ]
 
 var tickers: Array[Job] = [
@@ -58,28 +63,34 @@ var house_jobs: Array[Job] = [
 ]
 
 var curator_jobs: Array[Job] = [
-	load("res://Currencies/Library/DiscoverLibrary.tres"),
-	load("res://Currencies/Library/Get_Library_Card.tres"),
-	load("res://Currencies/Library/Speak_with_Curator.tres"),
-	load("res://Currencies/Library/Speak_with_Curator_2.tres"),
-	load("res://Currencies/Library/Speak_with_Curator_3.tres"),
-	load("res://Currencies/Library/Speak_with_Curator_4.tres"),
-	load("res://Currencies/Library/Offer_Kingly_Blue.tres"),
+	load("res://Currencies/Library/Curator/DiscoverLibrary.tres"),
+	load("res://Currencies/Library/Curator/Get_Library_Card.tres"),
+	load("res://Currencies/Library/Curator/Speak_with_Curator.tres"),
+	load("res://Currencies/Library/Curator/Speak_with_Curator_2.tres"),
+	load("res://Currencies/Library/Curator/Speak_with_Curator_3.tres"),
+	load("res://Currencies/Library/Curator/Speak_with_Curator_4.tres"),
+	load("res://Currencies/Library/Curator/Offer_Kingly_Blue.tres"),
+	load("res://Currencies/Library/Curator/Ask_Octomancy.tres"),
+	load("res://Currencies/Library/Curator/Ask_Octomancy_2.tres"),
+	load("res://Currencies/Library/Curator/Ask_Octomancy_3.tres"),
 ]
 
 var librarian_jobs: Array[Job] = [
-	load("res://Currencies/Library/Speak_with_librarian.tres"),
-	load("res://Currencies/Library/Speak_with_librarian2.tres"),
-	load("res://Currencies/Library/Speak_with_librarian3.tres"), 
-	load("res://Currencies/Library/Speak_with_librarian4.tres"),
+	load("res://Currencies/Library/Librarian/Speak_with_librarian.tres"),
+	load("res://Currencies/Library/Librarian/Speak_with_librarian2.tres"),
+	load("res://Currencies/Library/Librarian/Speak_with_librarian3.tres"), 
+	load("res://Currencies/Library/Librarian/Speak_with_librarian4.tres"),
+	load("res://Currencies/Library/Librarian/Ask_Cyanomancy.tres"),
+	load("res://Currencies/Library/Librarian/Ask_Cyanomancy_2.tres"),
+	load("res://Currencies/Library/Librarian/Ask_Cyanomancy_3.tres"),
 ]
 
 var research_book_jobs: Array[Job] = [
-	load("res://Currencies/Library/Research_The_City.tres"),
-	load("res://Currencies/Library/Research_Octomancy.tres"),
-	load("res://Currencies/Library/Research_Lapidamancy.tres"),
-	load("res://Currencies/Library/Research_Cyanomancy.tres"),
-	load("res://Currencies/Library/Research_Oneiromancy.tres"),
+	load("res://Currencies/Library/Bookshelves/Research_The_City.tres"),
+	load("res://Currencies/Library/Bookshelves/Research_Octomancy.tres"),
+	load("res://Currencies/Library/Bookshelves/Research_Lapidamancy.tres"),
+	load("res://Currencies/Library/Bookshelves/Research_Cyanomancy.tres"),
+	load("res://Currencies/Library/Bookshelves/Research_Oneiromancy.tres"),
 ]
 
 var tower_jobs: Array[Job] = [
