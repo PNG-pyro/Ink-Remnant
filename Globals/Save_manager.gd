8e6742bad7f92ec06a0f7ad20902eefb8c9ebb8b
Neal <parrnim@gmail.com>
2026-04-05 09:23:56 -0500

    savestring

--- Globals/Save_manager.gd
+++ Globals/Save_manager.gd
@@ -14,45 +14,9 @@ var focus_gained_callback
 var focus_lost_callback
 
 
-#func _ready() -> void:
-	#if OS.get_name() == "Web":
-		#_setup_web_visibility()
-
 
 func load_autosave():
 	load(save_name_3)
-
-
-#func _setup_web_visibility():
-	## create godot callbacks that javascript can call
-	#focus_lost_callback = JavaScriptBridge.create_callback(_on_focus_lost)
-	#focus_gained_callback = JavaScriptBridge.create_callback(_on_focus_gained)
-	#
-	## expose them to javascript on the window object
-	#var window = JavaScriptBridge.get_interface("window")
-	#window.focus_lost = focus_lost_callback
-	#window.focus_gained = focus_gained_callback
-		#
-	## tell the browser to call them when visibility changes
-	#JavaScriptBridge.eval("""
-		#document.addEventListener('visibilitychange', function() {
-			#if (document.hidden) {
-				#window.focus_lost();
-		#} else {
-				#window.focus_gained();
-			#}
-		#});
-	#""")
-
-#func _on_focus_lost(_args):
-	#last_focus_time = Time.get_unix_time_from_system()
-#
-#
-#func _on_focus_gained(_args):
-	#if last_focus_time > 0:
-		#var elapsed = Time.get_unix_time_from_system() - last_focus_time
-		#apply_catch_up(elapsed)
-		#last_focus_time = 0.0
 
 
 func save(savename: String) -> SaveState:
@@ -65,7 +29,8 @@ func save(savename: String) -> SaveState:
 	
 	save_state.all_jobs = []
 	for job in JobManager.all_jobs:
-		save_state.all_jobs.append(job.duplicate())
+		#save_state.all_jobs.append(job.duplicate())
+		save_state.jobs_dict[job.job_name] = job.shows_up
 		
 	for button in get_tree().get_nodes_in_group("BarButtons"):
 		save_state.button_states[button.job_run.job_name] = button.visual_state
@@ -108,10 +73,10 @@ func load_save(save_to_load: SaveState) -> bool:
 				currency.has_been_seen = saved_currency.has_been_seen
 				SignalHub.resource_updated.emit(currency, currency.amount)
 
-	for saved_job in save_to_load.all_jobs:
+	for saved_job in save_to_load.jobs_dict:
 		for job in JobManager.all_jobs:
-			if saved_job.job_name == job.job_name:
-				job.shows_up = saved_job.shows_up
+			if saved_job == job.job_name:
+				job.shows_up = save_to_load.jobs_dict[saved_job]
 	
 	for button in get_tree().get_nodes_in_group("BarButtons"):
 		if button.job_run.job_name in save_to_load.button_states:
@@ -123,33 +88,7 @@ func load_save(save_to_load: SaveState) -> bool:
 	for currency in CurrencyManager.all_currencies:
 		currency.get_max()
 	
-	#if save_to_load.theme == true:
-		#get_tree().current_scene.set_dark()
-	#else:
-		#get_tree().current_scene.set_light()
 		
 
 	SceneManager.set_scene(SceneManager.Scene.CITY)
 	return true
-
-
-#func _notification(what):
-	#if OS.get_name() == "Web":
-		#return
-	#match what:
-		#NOTIFICATION_APPLICATION_FOCUS_OUT:
-			## window lost focus - keep running but maybe throttle fps
-			#Engine.max_fps = 10
-			#last_focus_time = Time.get_unix_time_from_system() 
-		#NOTIFICATION_APPLICATION_FOCUS_IN:
-			#Engine.max_fps = 60  # 0 = unlimited, back to normal
-			#if last_focus_time > 0:
-				#var elapsed = Time.get_unix_time_from_system() - last_focus_time
-				#apply_catch_up(elapsed)
-				#last_focus_time = 0.0
-
-
-#func apply_catch_up(seconds: float):
-	#var ticks_missed = floor(seconds)
-	#for i in ticks_missed:
-		#get_tree().current_scene.increment_currencies()
--- SaveLoad/SaveState.gd
+++ SaveLoad/SaveState.gd
@@ -4,6 +4,7 @@ class_name SaveState
 @export var all_currencies: Array[Currency] = (CurrencyManager.all_currencies)
 @export var ui_state: int = 1 #Stores the enum from SceneManager.Scene
 @export var all_jobs: Array[Job] = JobManager.all_jobs
+@export var jobs_dict: Dictionary = {}
 @export var mute: bool = false
 @export var volume: float = 0.0
 @export var market_visible: bool = false
